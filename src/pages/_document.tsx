de4e9aa7422c33db56e51b6cca527c507de36f81
Giuseppe <contact.giuseppe00@gmail.com>
2025-09-15 05:28:05 +0100

    Override NextScript for strict CSP nonce compliance on all script tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,33 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, { Html, Head, Main, DocumentContext, NextScript as DefaultNextScript } from 'next/document';
+import React from 'react';
+import { DocumentProps } from 'next/document';
 import cookie from 'cookie';
+
+class CustomNextScript extends React.Component<{ nonce: string }> {
+  render() {
+    // Render all Next.js scripts with nonce
+    // This covers __NEXT_DATA__, hydration, and all Next.js-injected scripts
+    // See: https://github.com/vercel/next.js/issues/31792
+    // We override render to inject nonce into every <script>
+    // @ts-ignore
+    const scripts = DefaultNextScript.prototype.render.call(this);
+    // Recursively inject nonce into all <script> tags
+    function injectNonce(node: any, nonce: string): any {
+      if (!node) return node;
+      if (Array.isArray(node)) return node.map(child => injectNonce(child, nonce));
+      if (node.type === 'script') {
+        return React.cloneElement(node, { nonce });
+      }
+      if (node.props && node.props.children) {
+        return React.cloneElement(node, {
+          children: injectNonce(node.props.children, nonce)
+        });
+      }
+      return node;
+    }
+    return injectNonce(scripts, this.props.nonce);
+  }
+}
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -34,7 +62,7 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          <NextScript nonce={nonce} />
+          <CustomNextScript nonce={nonce} />
         </body>
       </Html>
     );
