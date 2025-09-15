76f02235001c543b6d786ee325782178dcab0c7c
Giuseppe <contact.giuseppe00@gmail.com>
2025-09-15 12:02:49 +0100

    Relax CSP to allow 'unsafe-inline' for scripts and styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 
+
 const nextConfig = {
   poweredByHeader: false,
   images: {
@@ -13,6 +14,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
